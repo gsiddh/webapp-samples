5ed5d945c514561560f2ca8223c08d12584775ce
Siddharth Gupta <guptasiddharth@google.com>
2025-06-11 09:20:52 -0700

    vue fcm working

---
+++ vue-demo/public/firebase-messaging-sw.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging/sw";
+
+// Initialize the Firebase app in the service worker
+const firebaseConfig = {
+    apiKey: "AIzaSyD37s0ZJeXjVUQRhGlO1cKgmGwk0GvbF00",
+    authDomain: "mess-auto.firebaseapp.com",
+    projectId: "mess-auto",
+    storageBucket: "mess-auto.firebasestorage.app",
+    messagingSenderId: "324484105269",
+    appId: "1:324484105269:web:24f6ce7ea1428397f1dd7c",
+    measurementId: "G-10W3S7SNZ4"
+  };
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,5 +1,38 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import { initializeApp } from "firebase/app"
+import { getMessaging, getToken, onMessage } from "firebase/messaging"
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD37s0ZJeXjVUQRhGlO1cKgmGwk0GvbF00",
+  authDomain: "mess-auto.firebaseapp.com",
+  projectId: "mess-auto",
+  storageBucket: "mess-auto.firebasestorage.app",
+  messagingSenderId: "324484105269",
+  appId: "1:324484105269:web:24f6ce7ea1428397f1dd7c",
+  measurementId: "G-10W3S7SNZ4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const messaging = getMessaging(app)
+
+// Request permission and get token
+getToken(messaging, { vapidKey: 'BH2rCQkMjwmOTRH9jkjykkmx5xUx25cDweLmL6cTlbJznP_nxtivrycwO4ltmlX3TyiHQjGGjJtZJqADYefGtPE' }).then((currentToken) => {
+  if (currentToken) {
+    console.log("currentToken:", currentToken)
+  } else {
+    console.log('No registration token available. Request permission to generate one.')
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err)
+})
+
+// Handle foreground messages
+onMessage(messaging, (payload) => {
+  console.log('Message received. ', payload)
+})
 </script>
 
 <template>
